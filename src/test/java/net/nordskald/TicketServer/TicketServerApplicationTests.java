00f785599b77f44e3c5910b733726f373798138b
Johan Wedberg <wedberg.johan@outlook.com>
2021-06-14 23:20:57 +0200

    Updated to source with spring initializer.

---
+++ src/test/java/net/nordskald/TicketServer/TicketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nordskald.TicketServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
